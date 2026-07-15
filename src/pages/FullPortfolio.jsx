1543865fabed77111f4afea1f8ec7fcfc50b9d78
Maucol3633 <maucol3633@students.ecpi.com>
2026-07-15 12:41:58 -0400

    Updated graphic design gallery layout

---
+++ src/pages/FullPortfolio.jsx
@@ -0,0 +1,20 @@
+import "../styles/FullPortfolio.css";
+
+function FullPortfolio() {
+  return (
+    <section className="full-portfolio">
+      <div className="container">
+        <h1>Full Portfolio</h1>
+        <p>
+          Explore my complete collection of graphic design work, including
+          branding, logos, print materials, social media graphics, marketing
+          campaigns, web design concepts, and more.
+        </p>
+
+        {/* Portfolio content goes here */}
+      </div>
+    </section>
+  );
+}
+
+export default FullPortfolio;
